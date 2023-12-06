37914d10079eb89c5e17fee62a9a99e76c18a102
DeroPlayz <deroplayz@gmail.com>
2023-12-05 19:49:54 -0800

    Started it!

---
+++ src/Main.py
@@ -0,0 +1,10 @@
+# Welcome to Python!
+# Let's start with syntax. This is like the grammar for a coding language.
+# Different things have different rules.
+# For example, to declare a variable in Python, you do:
+let Python = "Python"
+# 'let' tells the program you're about to name something. The next thing you type is the name of the variable.
+# So if you want to print it, you do:
+print(Python)
+# Then, the '=' tells the program you're gonna assign the value of the variable.
+# So, run this file, and see what it does!
